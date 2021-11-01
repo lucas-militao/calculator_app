d8636ac8c8146c1b3f56fb87ce3aa870ae9849f6
lumilitas <lucasmengineer@edu.unifor.br>
2021-11-01 19:02:49 -0300

    Fourth and Fifth task completed

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,13 +11,19 @@ export function Home() {
   const [action, setAction] = useState('');
   const [prevValue, setPrevValue] = useState(0);
   const [resultOfLastOperation, setResultOfLastOperation] = useState(0);
+  const [operationConcluded, setOperationConcluded] = useState(false);
 
   function handlePressPad(value: string) {
     if (numberOnDisplay.length < 8 && !isNaN(+value)) {
-      if (numberOnDisplay.length === 0 && value !== '0') {
-        setNumberOnDisplay((prevState) => prevState + value);
-      } else {
-        setNumberOnDisplay((prevState) => prevState + value);
+      if (!operationConcluded) {
+        if (numberOnDisplay.length === 0 && value !== '0') {
+          setNumberOnDisplay((prevState) => prevState + value);
+        } else {
+          setNumberOnDisplay((prevState) => prevState + value);
+        }
+      } else if (operationConcluded) {
+        setNumberOnDisplay(value);
+        setOperationConcluded(false);
       }
     } else {
       //Botão de operação apertado
@@ -25,27 +31,49 @@ export function Home() {
         //Igualdade e ação armazenada
         if (resultOfLastOperation !== 0 && prevValue !== 0) {
           //o resultado da operação anterior e o último número inserido
-          console.log("o resultado da operação anterior e o último número inserido");
-
+          
           let result = doMathOperation(resultOfLastOperation, prevValue, action);
           setNumberOnDisplay(result.toString());
           setResultOfLastOperation(result);
+          setOperationConcluded(true);
         } else if (prevValue !== 0 && numberOnDisplay.length !== 0) {
           //os dois últimos números inseridos
-          console.log("os dois últimos números inseridos");
-
+          
           let inputValueConverted = parseInt(numberOnDisplay);
           let result = doMathOperation(inputValueConverted, prevValue, action);
           setNumberOnDisplay(result.toString());
           setResultOfLastOperation(result);
+          setOperationConcluded(true);
         } else if (prevValue !== 0) {
           //o último número inserido
-          console.log("o último número inserido");
 
           let result = doMathOperation(prevValue, prevValue, action);
           setNumberOnDisplay(result.toString());
           setResultOfLastOperation(result);
+          setOperationConcluded(true);
         }
+      } else if(value === 'C') {
+        if (numberOnDisplay.length !== 0) {
+          //Pagar display
+          setNumberOnDisplay("");
+        } else if (action.length !== null) {
+          //Apagar operação e pôr no display último número inserido
+          setAction("");
+          setNumberOnDisplay(prevValue.toString());
+          setPrevValue(0);
+        } else {
+          //Apagar último número inserido
+          setPrevValue(0);
+          setResultOfLastOperation(0);
+          setOperationConcluded(false);
+        }
+      } else if(value === 'CE') {
+        //Apagar tudo
+        setNumberOnDisplay("");
+        setAction("");
+        setPrevValue(0);
+        setResultOfLastOperation(0);
+        setOperationConcluded(false);
       } else {
         if (action.length === 0) {
           let inputValueConverted = parseInt(numberOnDisplay);
