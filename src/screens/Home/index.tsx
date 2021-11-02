e7e4e1099eb1dfbe382646436716a63a4bef54ff
lumilitas <lucasmengineer@edu.unifor.br>
2021-11-02 09:49:42 -0300

    Refactoring

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,37 +11,77 @@ export function Home() {
   const [action, setAction] = useState('');
   const [prevValue, setPrevValue] = useState(0);
   const [resultOfLastOperation, setResultOfLastOperation] = useState(0);
-  const [operationConcluded, setOperationConcluded] = useState(false);
-  const [error, setError] = useState(false);
+
+  function handleCButton() {
+    if (numberOnDisplay.length !== 0) {
+      //Pagar display
+      setNumberOnDisplay("");
+    } else if (action.length !== 0) {
+      //Apagar operação e pôr no display último número inserido
+      setAction("");
+      setNumberOnDisplay(prevValue.toString());
+      setPrevValue(0);
+    } else {
+      //Apagar último número inserido
+      setPrevValue(0);
+      setResultOfLastOperation(0);
+    }
+  }
+
+  function handleCEButton() {
+    //Apagar tudo
+    setNumberOnDisplay("");
+    setAction("");
+    setPrevValue(0);
+    setResultOfLastOperation(0);
+  }
+
+  function handleSignNumberButton() {
+    //Mudar sinal do número no display
+    let numberOnDisplayConverted = parseInt(numberOnDisplay);
+    setNumberOnDisplay(numberOnDisplayConverted > 0
+      ? (-Math.abs(numberOnDisplayConverted)).toString()
+      : Math.abs(numberOnDisplayConverted).toString());
+  }
+
+  function handleDecimalPointButton() {
+    if (!numberOnDisplay.includes(',')) {
+      setNumberOnDisplay(prevInput => prevInput + ',');
+    }
+  }
+
+  function handleOperationButton(action: string) {
+    let inputValueConverted = parseInt(numberOnDisplay);
+    setAction(action);
+    setPrevValue(inputValueConverted);
+    setNumberOnDisplay("");
+  }
 
   function handlePressPad(value: string) {
     if (!isNaN(+value)) {
       //O valor de entrada é um número
-      if (numberOnDisplay.length < 8 && !error) {
+      if (numberOnDisplay.length < 8) {
+        //O número possui menos de 8 dígitos
         setNumberOnDisplay(lastInput => lastInput + value);
       }
     } else {
       //O valor de entrada é uma operação
-      if (value === '=' && action.length !== 0 && !error) {
+      if (value === '=' && action.length !== 0) {
         //Igualdade e ação armazenada
-
         let result = 0;
         if (resultOfLastOperation !== 0 && prevValue !== 0) {
           //o resultado da operação anterior e o último número inserido
-          
           result = doMathOperation(resultOfLastOperation, prevValue, action);
           // setNumberOnDisplay(result.toString());
           setResultOfLastOperation(result);
         } else if (prevValue !== 0 && numberOnDisplay.length !== 0) {
           //os dois últimos números inseridos
-          
           let inputValueConverted = parseInt(numberOnDisplay);
           result = doMathOperation(inputValueConverted, prevValue, action);
           // setNumberOnDisplay(result.toString());
           setResultOfLastOperation(result);
         } else if (prevValue !== 0) {
           //o último número inserido
-
           result = doMathOperation(prevValue, prevValue, action);
           // setNumberOnDisplay(result.toString());
           setResultOfLastOperation(result);
@@ -51,42 +91,16 @@ export function Home() {
         setNumberOnDisplay(resultConverted.length < 8 ? resultConverted : 'ERR');
         
       } else if(value === 'C') {
-        if (numberOnDisplay.length !== 0) {
-          //Pagar display
-          setNumberOnDisplay("");
-        } else if (action.length !== 0) {
-          //Apagar operação e pôr no display último número inserido
-          setAction("");
-          setNumberOnDisplay(prevValue.toString());
-          setPrevValue(0);
-        } else {
-          //Apagar último número inserido
-          setPrevValue(0);
-          setResultOfLastOperation(0);
-          setOperationConcluded(false);
-        }
-
+        handleCButton();
       } else if(value === 'CE') {
-        //Apagar tudo
-        setNumberOnDisplay("");
-        setAction("");
-        setPrevValue(0);
-        setResultOfLastOperation(0);
-        setOperationConcluded(false);
-        setError(false);
+        handleCEButton();
       } else if(value === '+/-') {
-        //Mudar sinal do número no display
-        let numberOnDisplayConverted = parseInt(numberOnDisplay);
-        setNumberOnDisplay(numberOnDisplayConverted > 0
-          ? (-Math.abs(numberOnDisplayConverted)).toString()
-          : Math.abs(numberOnDisplayConverted).toString());
-
+        handleSignNumberButton();
+      } else if(value === ',') {
+        handleDecimalPointButton();
       } else {
         if (action.length === 0) {
-          let inputValueConverted = parseInt(numberOnDisplay);
-          setAction(value);
-          setPrevValue(inputValueConverted);
-          setNumberOnDisplay("");
+          handleOperationButton(value);
         }
       }
     }
